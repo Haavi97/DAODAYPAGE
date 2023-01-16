6bbf5f807620c9fac260c97f82cd96df889b1932
Haavi <fortin@taltech.ee>
2023-01-16 15:05:54 +0200

    Added pfp

---
+++ generate_img_import.py
@@ -0,0 +1,20 @@
+# Write a function that writes import in the form of: "import andrewGreeneImage from '@/images/avatars/andrew-greene.jpg'"" from a given path
+
+import os
+
+
+def generate_import(path):
+    # path = './images/avatars'
+    files = os.listdir(path)
+    for file in files:
+        file_name = file.split('.')[0]
+        file_name = file_name.replace('_', '')
+        file_name = file_name.split('-')
+        name = file_name[0]
+        # capitaliza first letter
+        fam_name = file_name[1][0].upper() + file_name[1][1:]
+        full_name = name + fam_name
+        print("import " + full_name + "Image from '@/images/avatars/" + file + "'")
+
+
+generate_import('./src/images/avatars')
