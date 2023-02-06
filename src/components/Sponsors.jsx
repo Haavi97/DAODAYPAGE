a9e97c1973ff2ca05693b1fb608bb5f9b71b43b3
Haavi <fortin@taltech.ee>
2023-02-06 12:17:36 +0200

    Small changes

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,14 +1,20 @@
 import Image from 'next/image'
 
 import { Container } from '@/components/Container'
-import logoINO from '@/images/logos/logoINO2.png'
-import logoLegalLab from '@/images/logos/TalTechLegalLabHorizontal.png'
-import logoEResidency from '@/images/logos/e-residency_official-2.png'
-import logoChainalysis from '@/images/logos/Chainalysis-Logo.png'
+import logoINO from '@/images/logos/ino.png'
+import logoLegalLab from '@/images/logos/taltech-legal-lab.png'
+import logoEResidency from '@/images/logos/eresidency.png'
+import logoChainalysis from '@/images/logos/chainalysis.png'
+// import logoINO from '@/images/logos/logoINO2.png'
+// import logoLegalLab from '@/images/logos/TalTechLegalLabHorizontal.png'
+// import logoEResidency from '@/images/logos/e-residency_official-2.png'
+// import logoChainalysis from '@/images/logos/Chainalysis-Logo.png'
 // import logoUnigrid from '@/images/logos/Unigrid-Logo.png'
 // import logoDelfi from '@/images/logos/Delfi-Logo.png'
-import logoFounderly from '@/images/logos/founderly-logo.png'
-import logoEFT from '@/images/logos/EasyFeedback-logo.png'
+// import logoFounderly from '@/images/logos/founderly-logo.png'
+// import logoEFT from '@/images/logos/EasyFeedback-logo.png'
+import logoFounderly from '@/images/logos/founderly.png'
+import logoEFT from '@/images/logos/easyFeedback.png'
 
 const sponsors = [
   { name: 'INO', logo: logoINO },
