31adf647f5b41d4a433114891ab97f03a12f5060
Haavi <fortin@taltech.ee>
2023-03-02 11:09:38 +0200

    Added Privacy, Terms and conditions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { Container } from '@/components/Container'
 import { Logo } from '@/components/Logo'
+import Link from 'next/link'
 
 export function Footer() {
   return (
@@ -7,12 +8,30 @@ export function Footer() {
       <Container className="flex flex-col items-center justify-between md:flex-row">
         <Logo className="h-16 w-auto text-slate-900" />
         <div className="mt-6 text-base text-slate-500 md:mt-0">
-          <p>Organized by{' '}
-            <a class="tex-slate-500 hover:text-blue-600" target="_blank" rel="noreferrer noopener" href="https://internetnative.org?ref=daoday">Internet Native Organization.</a>
+          <p>
+            Organized by{' '}
+            <a
+              class="tex-slate-500 hover:text-blue-600"
+              target="_blank"
+              rel="noreferrer noopener"
+              href="https://internetnative.org?ref=daoday"
+            >
+              Internet Native Organization.
+            </a>
           </p>
-          <p class="text-center lg:text-right text-sm space-x-3">
-            <a class="tex-slate-500 hover:text-blue-600" href="#">Terms</a>
-            <a class="tex-slate-500 hover:text-blue-600" href="#">Privacy</a>
+          <p class="space-x-3 text-center text-sm lg:text-right">
+            <Link
+              class="tex-slate-500 hover:text-blue-600"
+              href="/terms-and-conditions"
+            >
+              Terms
+            </Link>
+            <Link
+              class="tex-slate-500 hover:text-blue-600"
+              href="/privacy-policy"
+            >
+              Privacy
+            </Link>
           </p>
         </div>
       </Container>
---
+++ src/pages/terms-and-conditions.jsx
@@ -0,0 +1,97 @@
+import Head from 'next/head'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+
+export default function TnC() {
+    return (
+      <>
+        <Head>
+          <title>Terms and Conditions - DAO Day Estonia</title>
+          <meta name="description" content="" />
+          <meta name="robots" content="noindex, nofollow" />
+        </Head>
+        <Header />
+        <main>
+        <div className="bg-white py-32 px-6 lg:px-8">
+            <div className="mx-auto max-w-5xl text-base leading-7 text-blue-900">
+                <h1 className="text-3xl font-bold tracking-tight text-blue-600 sm:text-4xl">Terms and Conditions</h1>
+                <p className="mt-6 text-base leading-8">
+                DAO Day Estonia is organized by INO MTÜ is a non-profit organization founded with the goal of finding ways to reduce legal risks and test the suitability of Estonia’s legal framework for DAOs, as well as develop tooling and legal wrappers for DAOs by leveraging Estonia’s digital residency and identity system.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                This document sets out the terms and conditions between INO MTÜ and DAO Day Estonia conference ticket holders.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                By purchasing a ticket for DAO Day Estonia conference you are agreeing to comply with and be bound by these terms and conditions.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                INO MTÜ reserves the right to change these terms and conditions at any time. Any amended terms and conditions will be posted to this website.
+                </p>
+                <h2 className="mt-16 text-2xl font-bold tracking-tight text-blue-900">Ticket prices</h2>
+                <p className="mt-6 text-base leading-8">
+                All prices indicated on the daoday.ee website are in euros and do not include Estonian VAT 20%
+                </p>
+                <p className="mt-6 text-base leading-8">
+                Student tickets are only valid in combination with a valid student ID. This must be presented at the badge pick-up and shown on request to gain admission to the event venue. If no such verification is provided, the purchased ticket is invalid, and the person will not be granted access to the event.
+                </p>
+                <h2 className="mt-16 text-2xl font-bold tracking-tight text-blue-900">Refund and cancellation policy</h2>
+                <p className="mt-6 text-base leading-8">
+                All tickets are non-refundable. The right of withdrawal for the ticket buyers is not applicable.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                However, it is possible to allocate the ticket to a third party if the ticket has been fully paid for. Ticket holder name, company or any other information can be changed by the person who purchased the ticket changing the data on the Ticket order by clicking on the Ticket order number in the confirmation email. Each new ticket holder will then receive the confirmation e-mail.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                Please note that changes to ticket holder details can be made only until 15 days prior to the event.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                The purchased tickets are for your personal or business use and may not be resold or transferred for commercial purposes under any circumstances. In case of resale or attempted resale of tickets (or any other breach of this condition), INO MTÜ reserves the right to cancel the relevant tickets with immediate effect.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                INO MTÜ is not responsible for granting visas necessary for participation in the DAO Day Estonia conference. If a participant’s visa is not granted, tickets for any event or conference will not be refunded.
+                </p>
+                <h2 className="mt-16 text-2xl font-bold tracking-tight text-blue-900">Terms of payment</h2>
+                <p className="mt-6 text-base leading-8">
+                DAO Day Estonia accepts payments only in euros.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                The following methods of payment are available:
+                </p>
+                <ul className="mt-6 text-base leading-8 list-disc list-inside">
+                  <li>Visa, MasterCard, American Express, Maestro, Apple Pay, Google Pay, Revolut worldwide;</li>
+                  <li>Sofort, iDEAL and local bank payments in selection of countries.</li>
+                </ul>
+                <p className="mt-6 text-base leading-8">
+                INO MTÜ may request verification of payment for the purpose of resolving possible disputes about the purchase.
+                </p>
+                <h2 className="mt-16 text-2xl font-bold tracking-tight text-blue-900">Entering the event</h2>
+                <p className="mt-6 text-base leading-8">
+                After purchasing a ticket for the DAO Day Estonia conference you will receive an automatically generated registration confirmation and the tickets. This confirmation including a unique bar code should be kept for acclaiming your DAO Day Estonia wristband that will grant access to the DAO Day Estonia conference.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                The pick-up locations will be located onsite at the event venue, where the latter will be communicated by email at least 4 weeks prior to the event. When picking up the wristband, the ticket buyer must present the e-mail ticket with the unique bar code as well as a valid ID.
+                </p>
+                <h2 className="mt-16 text-2xl font-bold tracking-tight text-blue-900">Event cancellation, postponement & substitution policy</h2>
+                <p className="mt-6 text-base leading-8">
+                Although speakers and topics are confirmed at least 1 month before the DAO Day Estonia conference, circumstances beyond the control of the organizers may require replacement, modification or cancellation of speakers and/or topics. As such, INO MTÜ reserves the right to change or substitute the speakers and/or topics being advertised as necessary without any liability to you. Any substitutions or changes will be updated on our website as soon as possible.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                If the event is canceled due to unforeseen circumstances (force majeure), ticket buyers will be refunded the total price of the purchased ticket(s). INO MTÜ is not responsible for other expenses incurred.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                In case of rescheduling DAO Day Estonia conference, tickets may be returned by a deadline to be determined by INO MTÜ.
+                </p>
+                <h2 className="mt-16 text-2xl font-bold tracking-tight text-blue-900">Liability</h2>
+                <p className="mt-6 text-base leading-8">
+                INO MTÜ shall not be liable for any loss, injury or damage to any person or property, however caused. INO MTÜ is not responsible for lost or stolen tickets. Each participant must take necessary measures and make provision for his/her own insurance.
+                </p>
+                <p className="mt-6 text-base leading-8">
+                INO MTÜ reserves the right to refuse admission to any event or conference for any behavior it deems unacceptable or for breach of these terms and conditions.
+                </p>
+            </div>
+        </div>
+        </main>
+        <Footer />
+      </>
+    )
+  }
