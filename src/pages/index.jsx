25f3eca6faf6c94852489fbee79bd5d58083c143
Haavi <fortin@taltech.ee>
2023-03-03 17:04:43 +0200

    Meta image

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,8 +26,7 @@ export default function Home() {
           property="og:description"
           content="A full day of expert-led presentations and panel discussions that will provide you with an in-depth introduction to the internet-first organizational structure."
         />
-        <meta property="og:image" content="/dao-day-estonia.png" />
-
+        <meta name="image" property="og:image" content="/dao-day-estonia.png" />
       </Head>
       <Header />
       {/* <NavBubble /> */}
