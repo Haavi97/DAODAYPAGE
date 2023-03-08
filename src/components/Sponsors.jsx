7e4f1b9db88119982499273f2f346d1476893440
Haavi <fortin@taltech.ee>
2023-03-08 08:14:20 +0200

    Updated sponsors

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -3,14 +3,20 @@ import Image from 'next/image'
 import { Container } from '@/components/Container'
 import logoINO from '@/images/logos/ino.svg'
 import logoFounderly from '@/images/logos/founderly.svg'
+import logoPWC from '@/images/logos/pwc.svg'
+import logoMinterest from '@/images/logos/minterest.svg'
 import logoCoinsPaid from '@/images/logos/coinspaid.svg'
 import logoChainalysis from '@/images/logos/chainalysis.svg'
+import logoEasyFeedbackToken from '@/images/logos/easyfeedbacktoken.svg'
 
 const sponsors = [
   { name: 'Internet Native Organization', logo: logoINO },
   { name: 'Founderly', logo: logoFounderly },
+  { name: 'PwC Legal', logo: logoPWC },
+  { name: 'Minterest', logo: logoMinterest },
   { name: 'CoinsPaid', logo: logoCoinsPaid },
   { name: 'Chainalysis', logo: logoChainalysis },
+  { name: 'Easy Feedback Token', logo: logoEasyFeedbackToken },
 ]
 
 export function Sponsors() {
@@ -20,13 +26,13 @@ export function Sponsors() {
         <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-blue-900 sm:text-5xl">
           Organizers and Supporters
         </h2>
-        <div className="mx-auto mt-20 grid max-w-max grid-cols-1 place-content-center gap-y-12 gap-x-32 sm:grid-cols-4 md:gap-x-16 lg:gap-x-32">
+        <div className="mx-auto mt-20 grid max-w-max grid-cols-2 place-content-center gap-y-12 gap-x-16 sm:grid-cols-3 lg:gap-x-32">
           {sponsors.map((sponsor) => (
             <div
               key={sponsor.name}
-              className="flex items-center justify-center"
+              className={"flex items-center justify-center " + (sponsor.name === 'Internet Native Organization' ? "sm:col-span-3 col-span-2" : "")}
             >
-              <Image height={sponsor.name === 'Internet Native Organization' ? 75 : 50} src={sponsor.logo} alt={sponsor.name} unoptimized />
+              <Image height={sponsor.name === 'Internet Native Organization' ? 150 : 75} src={sponsor.logo} alt={sponsor.name} unoptimized />
             </div>
           ))}
         </div>
