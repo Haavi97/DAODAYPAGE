5252a247622487dd89e35f0d4689bc71db36e6df
Haavi <fortin@taltech.ee>
2023-03-11 10:20:36 +0200

    Small updates in header. Organizer, contact

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,20 +19,20 @@ export function Header() {
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-blue-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
           <div className="mx-auto flex items-center gap-4 px-4">
             <a
-              href="https://info.internetnative.org/dd-expertise"
+              href="https://internetnative.org/about/"
               target="_blank"
               rel="noreferrer noopener"
               className="inline-block rounded-lg py-1 px-2 text-sm text-blue-600 hover:bg-blue-100 hover:text-blue-900"
             >
-              Apply to Speak
+              About the organizer
             </a>
             <a
-              href="https://info.internetnative.org/dd-volunteers"
+              href="https://internetnative.org/contact/"
               target="_blank"
               rel="noreferrer noopener"
               className="inline-block rounded-lg py-1 px-2 text-sm text-blue-600 hover:bg-blue-100 hover:text-blue-900"
             >
-              Apply to Volunteer
+              Contact us
             </a>
           </div>
         </div>
